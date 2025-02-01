cb529c3fd5939abc99c2d682eb2ba0d0168b69a4
karem-ex <keremcif19@gmail.com>
2025-02-01 11:04:04 +0300

    Create Problem2.c

---
+++ Play_with_Printf/Problem2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+// Diamond Pattern
+  printf("    *\n");
+  printf("   ***\n");
+  printf("  *****\n");
+  printf(" *******\n");
+  printf("*********\n");
+  printf(" *******\n");
+  printf("  *****\n");
+  printf("   ***\n");
+  printf("    *\n");
+  
+  return 0;
+
+}
