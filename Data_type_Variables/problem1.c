d26c956bca4ee5785f6f4ad161e39464143a9ea2
karem-ex <keremcif19@gmail.com>
2025-02-02 11:32:36 +0300

    Create problem1.c

---
+++ Data_type_Variables/problem1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int a = 10, b = 20, c = 30, sum; // Declare and initialize variables globally
+
+int main()
+{
+    sum = a + b + c;        // Calculate sum
+    printf("Sum: %d", sum); // Print the sum
+    return 0;               // Return 0 to indicate successful completion
+}
