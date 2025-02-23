92475234d7b49f3b774a414949d7fc9ab4e5d950
karem-ex <keremcif19@gmail.com>
2025-02-23 12:55:14 +0300

    Create Problem3.c

---
+++ Play_with_Printf/Problem3.c
@@ -0,0 +1,15 @@
+/*
+Question 3: Write a program to print the following output:
+    Name: x
+    Surname: y
+*/
+#include <stdio.h>
+
+
+int main(){
+
+    printf("Name: x \n");
+    printf("Surname: y");
+
+    return 0;
+}
