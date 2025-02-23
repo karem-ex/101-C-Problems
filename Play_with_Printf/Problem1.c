55f2231cbeea89ec1070f543b5c2dd956b936d72
karem-ex <keremcif19@gmail.com>
2025-02-23 12:54:10 +0300

    Create Problem1.c

---
+++ Play_with_Printf/Problem1.c
@@ -0,0 +1,15 @@
+/*
+Question 1: Write the skeleton of a C program. 
+*/
+#include <stdio.h> // This is the header file that contains some standard functions and rule sets.
+
+
+int main(){
+    /*
+    
+    Main block which should be in every program
+    
+    */
+
+    return 0; // For the reason of return type , we need some integner to return.
+}
