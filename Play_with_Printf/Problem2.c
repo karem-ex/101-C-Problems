aa2406192e2b41e37ba71df036b5db87917435d4
karem-ex <keremcif19@gmail.com>
2025-02-23 12:54:46 +0300

    Create Problem2.c

---
+++ Play_with_Printf/Problem2.c
@@ -0,0 +1,13 @@
+/*
+Question 2: Write a program to print the following output: 
+Hello World !!!
+*/
+#include <stdio.h>
+
+
+int main(){
+
+    printf("Hello World !!!"); //printf() is a function , used to print out on the screen.
+
+    return 0;
+}
