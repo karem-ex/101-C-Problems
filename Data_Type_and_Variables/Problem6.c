4a7988b4cdaa7cef3d18123f13697080b92a3d28
karem-ex <keremcif19@gmail.com>
2025-02-24 08:43:10 +0300

    Create Problem6.c

---
+++ Data_Type_and_Variables/Problem6.c
@@ -0,0 +1,21 @@
+/*
+Question 6: Take three integner variables a,b and c. Assign values to the variables;
+            a = 10 b = 20 c = 30
+            Now print the sum of these three variables.
+*/
+#include <stdio.h>
+
+int a ,b ,c,total;
+
+
+int main(){
+
+    a = 10;
+    b = 20;
+    c = 30;
+    total = a + b + c;
+    printf("Sum of three variables: %d",total);
+
+    return 0;
+}
+
