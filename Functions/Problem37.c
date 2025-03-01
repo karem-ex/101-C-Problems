a27252509bb87eadcecde2aff853e4e50c94603f
karem-ex <keremcif19@gmail.com>
2025-03-01 22:37:18 +0300

    Create Problem37.c

---
+++ Functions/Problem37.c
@@ -0,0 +1,34 @@
+/*
+
+Question 37: Syntax function of c programming language.
+
+
+type_of_function Name_of_Function(int param, int *params[]){
+
+
+        block of code
+
+}
+
+*/
+#include <stdio.h>
+// This is the main function of the c programming language.
+/*
+ The main function in the C programming language is the starting point of every C program. 
+ When the program is run, the operating system first calls the main function. 
+ This function is where the execution of the program begins.
+*/
+
+void print_(){
+
+
+    printf("Hello World!!");
+}
+
+
+int main(){
+
+    print_();
+
+    return 0; // 0 indicates that the program ran successfully.
+}
