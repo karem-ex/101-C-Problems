45e2599014777d31d4e102c0c1a789c1f9c73a90
karem-ex <keremcif19@gmail.com>
2025-03-05 21:56:24 +0300

    Create Problem48.c

---
+++ Pointers/Problem48.c
@@ -0,0 +1,13 @@
+/*
+Question 48: Take an integner variable named X and assign x = 5. Now print its memory address like the following output.
+
+Screen to print: Memory Address of X is: XXXXXX
+*/
+
+#include <stdio.h>
+int x = 5;
+
+void main(){
+
+    printf("Memory Address: %d",&x);
+}
